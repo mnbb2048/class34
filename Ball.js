872d51d50f3298f2b232ddbc2c505c54e6a8e0fa
mnbb2048 <76206769+mnbb2048@users.noreply.github.com>
2021-06-01 14:48:41 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,22 @@
+class Ball{
+    constructor(x,y,width,height){
+        var options = {
+            density:1,
+            frictionAir:0.005 
+           }
+           this.body = Bodies.rectangle(x, y, width, height, options);
+           this.width = width;
+           this.height = height;
+           World.add(world, this.body);  
+    }
+    display(){
+        push();
+        translate(this.body.position.x,this.body.position.y)
+        rotate(this.body.angle)
+        strokeWeight(5)
+        stroke("purple")
+        fill("brown")
+        ellipse(0,0,this.width,this.height)
+       pop()
+      }
+}
---
+++ Box.js
@@ -0,0 +1,22 @@
+class Box{
+  constructor(x, y, width, height) {
+      var options = {
+       restitution:0.5            
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+      
+    }
+    display(){
+      push();
+      translate(this.body.position.x,this.body.position.y)
+      rotate(this.body.angle)
+      strokeWeight(5)
+      stroke("green")
+      fill("white")
+      rect(0,0,this.width,this.height)
+     pop()
+    }
+}
